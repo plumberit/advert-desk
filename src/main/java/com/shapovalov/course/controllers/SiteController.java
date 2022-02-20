0ba2d8b8f933823178c01ab5c6c640bb1f860c3a
plumberit <dshapovalov2001@gmail.com>
2022-02-20 20:57:00 +0200

    create model entities

--- src/main/java/com/shapovalov/course/controllers/SiteController.java
+++ src/main/java/com/shapovalov/course/controllers/SiteController.java
@@ -3,17 +3,15 @@ package com.shapovalov.course.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import java.util.ArrayList;
-
 
 @Controller
 public class SiteController {
-
     @GetMapping("/")
     public String getMainPage(Model model) {
         Iterable<Client> guests = clientRepository.findAll();
-        model.addAttribute("guests", guests);
+        model.addAttribute('guests', guests);
+
         return "system templates/white at start";
     }
 
---
+++ src/main/java/com/shapovalov/course/model/Advertisement.java
@@ -0,0 +1,63 @@
+package com.shapovalov.course.model;
+
+import javax.persistence.*;
+
+public class Advertisement {
+
+    private Long clientID;
+
+    private String name;
+
+    private String surname;
+
+    private String communication;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getClientID() {
+        return clientID;
+    }
+
+    public void setClientID(Long clientID) {
+        this.clientID = clientID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getCommunication() {
+        return communication;
+    }
+
+    public void setCommunication(String communication) {
+        this.communication = communication;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
