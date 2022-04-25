04e2177ccbadeec57dcd108025df24760ff347a2
plumberit <dshapovalov2001@gmail.com>
2022-04-25 14:07:00 +0300

    create extended advertisment in service

---
+++ src/main/java/com/shapovalov/course/service/ExtendedAdvertisement.java
@@ -0,0 +1,73 @@
+package com.shapovalov.course.service;
+
+public class ExtendedAdvertisement {
+    private Long id;
+
+    private Long clientID;
+
+    private String name;
+
+    private String surname;
+
+    private String communication;
+
+    private String description;
+
+    private String briefDescription;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getClientID() {
+        return clientID;
+    }
+
+    public void setClientID(Long clientID) {
+        this.clientID = clientID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getCommunication() {
+        return communication;
+    }
+
+    public void setCommunication(String communication) {
+        this.communication = communication;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBriefDescription() {
+        return briefDescription;
+    }
+
+    public void setBriefDescription(String briefDescription) {
+        this.briefDescription = briefDescription;
+    }
+}
